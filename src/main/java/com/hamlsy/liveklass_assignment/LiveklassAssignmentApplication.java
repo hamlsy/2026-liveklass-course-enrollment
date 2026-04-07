ebf759a85716189fd436fa703e03f332ee8584f5
hamlsy <dltmddud1122@naver.com>
2026-04-07 11:23:43 +0900

    Initial commit: LiveKlass enrollment system setup

---
+++ src/main/java/com/hamlsy/liveklass_assignment/LiveklassAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.hamlsy.liveklass_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveklassAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveklassAssignmentApplication.class, args);
+	}
+
+}
